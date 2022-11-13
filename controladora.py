52d3f1aba86147f81eccff2e29f5d614cbf6f1f4
AndersonGamarraV <89112814+AndersonGamarraV@users.noreply.github.com>
2022-11-13 13:22:11 -0500

    Update controladora.py
    Se implemento la cantidad de entidades

--- controladora.py
+++ controladora.py
@@ -1,7 +1,12 @@
 class Controladora:
     def __init__(self) -> None:
         self.__time = 0.0
+        self.__entidades = 0
     
     @property
     def time(self) -> float:
         return self.__time
+    
+    @property
+    def entidades(self) -> int:
+        return self.__entidades
